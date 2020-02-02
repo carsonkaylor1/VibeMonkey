f574346c8a5abfa18a56e9acfddb53da7fc0ffe3
carsonkaylor1 <carsonkaylor@gmail.com>
2020-02-01 19:30:04 -0500

    Updates

---
+++ precache-manifest.33699535b0a51677f9333e735a2f7e2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b204e099ec7bbad0d6ff09f87a7822c4",
+    "url": "/VibeMonk/index.html"
+  },
+  {
+    "revision": "0cc76c96728a3acabab3",
+    "url": "/VibeMonk/static/css/main.f5b02229.chunk.css"
+  },
+  {
+    "revision": "e0111c237b1a6da59a29",
+    "url": "/VibeMonk/static/js/2.003a8318.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/VibeMonk/static/js/2.003a8318.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0cc76c96728a3acabab3",
+    "url": "/VibeMonk/static/js/main.7e85dcb7.chunk.js"
+  },
+  {
+    "revision": "e1adbe291443218eebd8",
+    "url": "/VibeMonk/static/js/runtime-main.53508009.js"
+  }
+]);
